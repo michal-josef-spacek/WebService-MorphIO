53db263c24cf74d771479362a11a805ceecf9bd8
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2020-07-16 15:24:08 +0200

    Update author name

--- MorphIO.pm
+++ MorphIO.pm
@@ -225,13 +225,13 @@ L<https://github.com/tupinek/WebService-MorphIO>
 
 =head1 AUTHOR
 
-Michal Špaček L<mailto:skim@cpan.org>
+Michal Josef Špaček L<mailto:skim@cpan.org>
 
 L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
- © 2014-2015 Michal Špaček
+ © 2014-2015 Michal Josef Špaček
  BSD 2-Clause License
 
 =head1 VERSION
